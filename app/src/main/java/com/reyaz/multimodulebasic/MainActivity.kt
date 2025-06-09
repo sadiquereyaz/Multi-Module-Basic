5312cc010f53f70607efc98886d85858b0e9bc62
sadiquereyaz <mdsadique47@gmail.com>
2025-06-09 16:47:10 +0530

    feat: Initialize basic Android project structure
    This commit introduces the foundational files and configurations for a new Android application.
    
    Key additions include:
    - `.gitignore` files for the project root, `.idea` directory, and `app` module to exclude build artifacts and IDE-specific files.
    - `AndroidManifest.xml` defining the application's components, permissions, and basic properties.
    - Gradle build scripts (`build.gradle.kts` for both project and app module, `settings.gradle.kts`, `gradle.properties`, `gradle-wrapper.properties`, `gradlew`, `gradlew.bat`, `libs.versions.toml`) to manage dependencies and build processes.
    - Basic resource files such as launcher icons, colors, strings, themes, and backup rules.
    - Initial source code including `MainActivity.kt`, UI theme files (`Color.kt`, `Theme.kt`, `Type.kt`), and example unit/instrumented tests.
    - IDE configuration files (`.idea` directory) for project settings, compiler options, run configurations, VCS integration, and inspection profiles.

---
+++ app/src/main/java/com/reyaz/multimodulebasic/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.reyaz.multimodulebasic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.reyaz.multimodulebasic.ui.theme.MultiModuleBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MultiModuleBasicTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MultiModuleBasicTheme {
+        Greeting("Android")
+    }
+}
